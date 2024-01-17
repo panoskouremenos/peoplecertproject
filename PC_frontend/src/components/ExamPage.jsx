6433a43471cbd10360c6a53d9552a7d3390811dc
Kyriakos Passakos <darkos13@hotmail.com>
2024-01-18 01:14:48 +0200

    Exam component 90% done

--- PC_frontend/src/components/ExamPage.jsx
+++ PC_frontend/src/components/ExamPage.jsx
@@ -8,6 +8,23 @@ const ExamPage = () => {
   const { token } = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
 
+
+  useEffect(() => {
+    const tickedquestions = JSON.parse(localStorage.getItem("tickedquestions"));
+    console.log('edo1');
+    console.log(tickedquestions);
+    if (tickedquestions) {
+      console.log('localstorage loading into the questions state');
+      setQuestions(tickedquestions);
+      console.log('here is questions state:')
+      console.log(questions);
+      setIsLoading(false);
+    }else{
+      fetchExamQuestions(11);
+    }
+
+  }, [token]);
+
   function convertNumberToLetter(number) {
     if (number >= 0 && number <= 25) {
       return String.fromCharCode(65 + number);
@@ -15,17 +32,24 @@ const ExamPage = () => {
       return null;
     }
   }
+
   const handleRadioClick = (index) => {
     setQuestions((prevQuestions) => {
       const updatedQuestions = [...prevQuestions];
       updatedQuestions[activeQuestion].checkedAnswer = index;
       return updatedQuestions;
     });
+    setTimeout(function(){
+      localStorage.setItem('tickedquestions' , JSON.stringify(questions));
+    },50)
+    console.log('localstorage updated :');
+    console.log(JSON.parse(localStorage.getItem('tickedquestions')))
   };
   
   const handleChangeQuestion = (index) => {
     setActiveQuestion(index);
   }
+
   const fetchExamQuestions = async (id) => {
     try {
       const response = await fetch(`https://localhost:5888/api/Certificates/${id}`, {
@@ -58,10 +82,6 @@ const ExamPage = () => {
     }
   };
 
-  useEffect(() => {
-    fetchExamQuestions(11);
-  }, [token]);
-
   return (
     <div id="exampage" className="container mt-5">
       <div className="row">
@@ -73,8 +93,7 @@ const ExamPage = () => {
           <div className="list-group" style={{overflowY : "scroll" , maxHeight : "80%" , padding : "1em" }}>
             {questions.map((q , i) => {
                   return (
-                    //questions[activeQuestion].checkedAnswer === false
-                    <button onClick={(e) => handleChangeQuestion(i)} key={i} type="button" className={`${questions[activeQuestion].checkedAnswer === false ? "isnotchecked" : "ischecked"} list-group-item list-group-item-action ${activeQuestion === i ? "active" : ""}`}>
+                    <button onClick={(e) => handleChangeQuestion(i)} key={i} type="button" className={`${questions[activeQuestion].checkedAnswer === false ? "notchecked" : "" } ${questions[activeQuestion].checkAnswer !== false ? "checked" : ""} list-group-item list-group-item-action ${activeQuestion === i ? "active" : ""}`}>
                     Question {i}
                   </button>
                   );
@@ -82,30 +101,30 @@ const ExamPage = () => {
           </div>
         </div>
         <div className="col-md-9 d-flex flex-column align-items-center">
-          <div className="border p-3">
-            <h4>MCQ Quiz</h4>
+          <div className="border p-3 d-flex flex-column justify-content-center align-items-center">
+            <h2 className="text-center">MCQ Quiz</h2>
             <div className="d-flex flex-row align-items-center question-title">
               <h3 className="text-danger">{questions[activeQuestion].questionText}</h3>
             </div>
             <div className="ans ml-2">
             {questions[activeQuestion].possibleAnswers.split(',').map((_ , i) => {
               return (
-                <label className="options">{convertNumberToLetter(i)} : {questions[activeQuestion].possibleAnswers.split(',')[i]}
+                <label className="options" key={i}>{convertNumberToLetter(i)} : {questions[activeQuestion].possibleAnswers.split(',')[i]}
                  <input type="radio" name="radio" onClick={(e) => handleRadioClick(i)} checked={questions[activeQuestion].checkedAnswer === false ? "" : questions[activeQuestion].checkedAnswer === i}/>
                   <span className="checkmark"></span>
                 </label>)
             })
             }
             </div>
-            <div className="d-flex flex-row justify-content-between align-items-center p-3 bg-white">
+            <div className="p-3 bg-white gap-3" style={{ minWidth : "100%"}}>
               {activeQuestion > 0 && 
               <button onClick={(e) => setActiveQuestion(activeQuestion > 0 ? activeQuestion-1 : 0)} className="btn btn-primary d-flex align-items-center btn-danger" type="button">
-                <i className="fa fa-angle-left mt-1 mr-1"></i>&nbsp;previous
+                <i className="fa fa-angle-left mt-1 mr-1 float-left"></i>&nbsp;previous
               </button>
               }
               {activeQuestion < questions.length-1 &&
               <button onClick={(e) => setActiveQuestion(activeQuestion < questions.length ? activeQuestion+1 : 0)} className="btn btn-primary border-success align-items-center btn-success" type="button">
-                Next<i className="fa fa-angle-right ml-2"></i>
+                Next<i className="fa fa-angle-right ml-2 float-right"></i>
               </button>
               }
             </div>
